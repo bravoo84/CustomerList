d05c56fadf4b69116d28f0e2d99a5a57bd05f6b1
bhavik <bhavikmehta98@gmail.com>
2019-09-20 13:29:16 +0530

    part-2 remaining files

---
+++ src/com/bhavik/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.bhavik.springdemo.dao;
+
+import java.util.List;
+
+import com.bhavik.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
